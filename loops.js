a29d96909fc939f2192ae1de1b74eeb7a32f1b9f
Nora Gaughan <noragaughan@gmail.com>
2020-07-13 19:18:55 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -11,6 +11,10 @@ function forLoop(array) {
   }
   
   function whileLoop(number) {while (number > 0) {console.log(number--)} return "done"
+  
+  function maybeTrue() {
+  return Math.random() >= 0.5
+}
   }
   
 function doWhileLoop(array) {
